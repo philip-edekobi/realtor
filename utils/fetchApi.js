2e05b9dde3d769d1a223f388955f00281a0a556e
poposki317 <philipedekobi@gmail.com>
2022-02-04 06:53:48 +0100

    woosh

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com/';
 const options = {
   headers: {
     'x-rapidapi-host': 'bayut.p.rapidapi.com',
-    'x-rapidapi-key': '2dfd41baeamsh9a3b8654372e1bcp1b7dcejsn4cd1e5d3edf7'
+    'x-rapidapi-key': process.env.API_KEY
   }
 };
 
